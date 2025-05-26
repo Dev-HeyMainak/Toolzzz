fc9e71e050ee129f49289816ec24b9a046d78f33
Mainak <editing.mainak@gmail.com>
2025-05-26 18:42:45 +0000

    i told you to don't add anything that require any api

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,4 +1,6 @@
+
 // Flows will be imported for their side effects in this file.
 import "./flows/passwordGenerator";
-import "./flows/emailValidator"; // This now acts as a wrapper
-import "./flows/emailValidationFlow"; // Import the Genkit flow definitions
+import "./flows/emailValidator"; // This now acts as a wrapper for the mock flow
+
+// Removed: import "./flows/emailValidationFlow"; // This was the Genkit flow
